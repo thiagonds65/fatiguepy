8155d4110ea94f44f081d157df3869522f87fbc3
thiagonds65 <thiago@ubuntu.ubuntu-domain>
2020-06-24 20:48:10 -0300

    update __init__.py

--- fatiguepy/__init__.py
+++ fatiguepy/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['prob_moment', 'Rainflow', 
             'Narrow_Band', 'Wirsching_Light', 
-            'Dirlik', 'Zhao_Baker', 'alpha0dot75', 'Tovo_Bennasciutti']
+            'Dirlik', 'Zhao_Baker', 'alpha075', 'Tovo_Bennasciutti']
 
 ## __all__ list tell us what will be import with "from fatiguepy import *"
