c256834c5292f345e820894e55902a16eec0ce74
thiagonds65 <thiago@ubuntu.ubuntu-domain>
2020-08-22 17:17:03 -0300

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='fatiguepy',
-      version='1.9',
+      version='1.9.1',
       description='Package to estimate life of random fatigue history with frequency domain methods',
       long_description=long_description,
       long_description_content_type="text/markdown",
